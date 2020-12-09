2684586e73756dede72776181253aedf6e3606e8
JordanJStone <statictrack@gmail.com>
2020-12-09 15:17:04 -0700

    1st iteration of app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+console.log("merry christmas");
+
+let rock = 1
+let paper = 2
+let compChoice = 2
+let scissors = 3
+let compRandom = Math.floor(Math.random() * 3) + 1;
+
+
+
+function play(rock) {
+  if (rock < compChoice) {
+    return "rock wins"
+  } else if {
+    return "paper wins"
+  } else {
+    "it's a draw"
+  }
+}
+
+function play(paper) {
+  if (paper <= compChoice) {
+    return "paper wins"
+  } else if {
+    return "paper wins"
+  } else {
+    "it's a draw"
+  }
+}
+
+function play(scissors) {
+  if (scissors < compChoice) {
+    return "paper wins"
+  } else if {
+    return "scissors wins"
+  } else {
+    "it's a draw"
+  }
+}
